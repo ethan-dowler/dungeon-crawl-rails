aadf0b5cf9d89171f7d242da3193b06fafe3accd
Ethan Dowler <tedowler@gmail.com>
2023-12-13 23:17:08 -0600

    update rubocop config

--- app/components/character_summary_component.rb
+++ app/components/character_summary_component.rb
@@ -1,10 +1,11 @@
 class CharacterSummaryComponent < ViewComponent::Base
   attr_reader :character, :previous_hp, :show_status
-  alias show_status? show_status
+  alias_method :show_status?, :show_status
 
   delegate :name, :level, :current_hp, :max_hp, :xp, :xp_to_next_level, to: :character
 
-  # TODO: allow "previous character" param that is the character as it was before the current render. This will make it easy to animate all the things.
+  # TODO: allow "previous character" param that is the character as it was before the current render.
+  # This will make it easy to animate all the things.
   def initialize(character, previous_hp: nil, show_status: true)
     @character = character
     @previous_hp = previous_hp
