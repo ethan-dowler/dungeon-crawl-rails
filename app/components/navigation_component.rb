b12ab0b74b27d8d5166d980142ca274091f17038
Ethan Dowler <tedowler@gmail.com>
2024-07-04 23:32:03 -0500

    navigation styles

--- app/components/navigation_component.rb
+++ app/components/navigation_component.rb
@@ -7,23 +7,6 @@ class NavigationComponent < ApplicationComponent
     @dungeon_run = dungeon_run
   end
 
-  def direction_classes(direction) # rubocop:disable Metrics/MethodLength
-    case direction
-    when Dungeon::Direction::NORTH
-      "top-1 left-1/2 -translate-x-1/2"
-    when Dungeon::Direction::EAST
-      "right-1 top-1/2 -translate-y-1/2"
-    when Dungeon::Direction::SOUTH
-      "bottom-1 left-1/2 -translate-x-1/2"
-    when Dungeon::Direction::WEST
-      "left-1 top-1/2 -translate-y-1/2"
-    when Dungeon::Direction::ABOVE, Dungeon::Direction::BELOW
-      "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    else
-      ""
-    end
-  end
-
   def direction_text(direction)
     case direction
     when Dungeon::Direction::ABOVE
